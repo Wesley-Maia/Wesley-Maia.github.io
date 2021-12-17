f021874dc24bca8dd207d07d1e4791e60053c439
Wesley Maia <maia.weol@gmail.com>
2021-12-17 08:40:49 -0300

    Update format

---
+++ script.js
@@ -0,0 +1,38 @@
+// =======================================================================================
+// addEventListener
+
+const prj1 = document.getElementById('prj1');
+const prj2 = document.getElementById('prj2');
+const prj3 = document.getElementById('prj3');
+const myWebpage = document.getElementById('mySpotrybefy');
+
+// 1- Função que adiciona a classe 'tech' ao elemento selecionado.
+//  - Deve existir apenas um elemento com a classe 'tech'.
+function changeTech(event) {
+  const techElement = document.querySelector('.tech');
+  techElement.classList.remove('tech');
+  event.target.classList.add('tech');
+
+}
+prj1.addEventListener('click',changeTech);
+prj2.addEventListener('click',changeTech);
+prj3.addEventListener('click',changeTech);
+
+//  2- Função que, ao clicar duas vezes nos projetos, ele redireciona para o repositório do mesmo.
+function pageRedirect(event) {
+
+  if(event.target.id === 'prj1') {
+    window.location.replace('https://github.com/Wesley-Maia/trybe-exercises')
+  }
+
+  if(event.target.id === 'prj2') {
+    window.location.replace('https://github.com/Wesley-Maia/trybe-exercises')
+  }
+
+  if(event.target.id === 'prj3') {
+    window.location.replace('https://github.com/Wesley-Maia/trybe-exercises')
+  }
+}
+prj1.addEventListener('dblclick',pageRedirect);
+prj2.addEventListener('dblclick',pageRedirect);
+prj3.addEventListener('dblclick',pageRedirect);
